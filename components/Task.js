3700b1533a4a390595cd7128739f371142edb71c
bharathi29 <bharathibaskaran2902@gmail.com>
2024-10-25 20:47:09 +0530

    changes

--- components/Task.js
+++ components/Task.js
@@ -4,7 +4,13 @@ import { View,Text,StyleSheet } from "react-native";
 const Task = () => {
     return(
         <View>
-            
+            <Text>
+                This is a task
+            </Text>
         </View>
     )
 }
+
+const styles=StyleSheet.create()
+
+export default Task;
